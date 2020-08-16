55e906c479325d1e12fd870e73f6c0d44f5cd3af
ossleet91 <ossleet91@gmail.com>
2020-08-16 00:56:49 +0000

    Add array big sum exercise

--- arraySum.go
+++ arraySum.go
@@ -9,3 +9,13 @@ func arraySum(array []int32) int32 {
 
 	return sum
 }
+
+func arrayBigSum(array []int64) int64 {
+	var sum int64
+
+	for _, num := range array {
+		sum += num
+	}
+
+	return sum
+}
--- main.go
+++ main.go
@@ -13,6 +13,10 @@ func main() {
 	printLine()
 
 	var arrSumVals = []int32{1, 2, 3, 4, 10, 11}
-	fmt.Printf("Array sum of {%v} = %d\n", arrSumVals, arraySum([]int32{1, 2, 3, 4, 10, 11}))
+	fmt.Printf("Array sum of %v = %d\n", arrSumVals, arraySum(arrSumVals))
+	printLine()
+
+	var arrBigSumVals = []int64{1000000001, 1000000002, 1000000003, 1000000004, 1000000005}
+	fmt.Printf("Array big sum of %v = %d\n", arrBigSumVals, arrayBigSum(arrBigSumVals))
 	printLine()
 }
